0b76adc2d3af981c0206e6108143448351c626f6
Takecontrole <takecontrole2034@gmail.com>
2023-06-28 15:57:46 +0400

    test

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,35 +1,37 @@
-import axios from './axios'
+import axios from 'axios'
+const rootUrl = process.env.NODE_ENV === "production" ? "https://social-media-api.adaptable.app" : ""
 
+  
 export const getDataAPI = async (url, token) => {
-    const res = await axios.get(`/api/${url}`, {
+    const res = await axios.get(`${rootUrl}/api/${url}`, {
         headers: { Authorization: token}
     })
     return res;
 }
 
 export const postDataAPI = async (url, post, token) => {
-    const res = await axios.post(`/api/${url}`, post, {
+    const res = await axios.post(`${rootUrl}/api/${url}`, post, {
         headers: { Authorization: token}
     })
     return res;
 }
 
 export const putDataAPI = async (url, post, token) => {
-    const res = await axios.put(`/api/${url}`, post, {
+    const res = await axios.put(`${rootUrl}/api/${url}`, post, {
         headers: { Authorization: token}
     })
     return res;
 }
 
 export const patchDataAPI = async (url, post, token) => {
-    const res = await axios.patch(`/api/${url}`, post, {
+    const res = await axios.patch(`${rootUrl}/api/${url}`, post, {
         headers: { Authorization: token}
     })
     return res;
 }
 
 export const deleteDataAPI = async (url, token) => {
-    const res = await axios.delete(`/api/${url}`, {
+    const res = await axios.delete(`${rootUrl}/api/${url}`, {
         headers: { Authorization: token}
     })
     return res;
